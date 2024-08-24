405424139b62cc8336d8980ed2f303ecd85f0bd9
Your Name <you@example.com>
2024-08-24 05:50:33 +0000

    Change pre-apply 1724478633

--- plugins.nix
+++ plugins.nix
@@ -59,6 +59,30 @@
     })
   '';
   config.plugins = {
+    undotree = {
+      enable = true;
+      settings = {
+        CursorLine = true;
+        DiffAutoOpen = true;
+        DiffCommand = "diff";
+        DiffpanelHeight = 10;
+        HelpLine = true;
+        HighlightChangedText = true;
+        HighlightChangedWithSign = true;
+        HighlightSyntaxAdd = "DiffAdd";
+        HighlightSyntaxChange = "DiffChange";
+        HighlightSyntaxDel = "DiffDelete";
+        RelativeTimestamp = true;
+        SetFocusWhenToggle = true;
+        ShortIndicators = false;
+        SplitWidth = 40;
+        TreeNodeShape = "*";
+        TreeReturnShape = "\\";
+        TreeSplitShape = "/";
+        TreeVertShape = "|";
+        WindowLayout = 4;
+      };
+    };
     # https://github.com/sidebar-nvim/sidebar.nvim
     # sidebar-nvim.enable = true;
     cmp = {
