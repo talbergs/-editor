9b521ecf56e824273ce50e4e0e6b665db4c5634d
Your Name <you@example.com>
2024-09-06 20:01:48 +0000

    Change pre-apply 1725652908

---
+++ conf.nixvim.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  config,
+  helpers,
+  ...
+}:
+let
+
+  quicker = pkgs.vimUtils.buildVimPlugin {
+    name = "nvim-quicker";
+    src = pkgs.fetchFromGitHub {
+      owner = "stevearc";
+      repo = "quicker.nvim";
+      rev = "308088ebcec33f9ed551714e52390206b8f62ed6";
+      hash = "sha256-l2M4uVuQ+NW/Nf6fwGlBUqKiWzTld/tePMPMqk3W/oM=";
+    };
+  };
+  quicker_lua = ''
+    require("quicker").setup()
+  '';
+
+in
+{
+  config.extraConfigLua = quicker_lua;
+  config.extraPlugins = [ quicker ];
+}
