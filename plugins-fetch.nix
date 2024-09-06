4afb0f4daefba49005b5c776ef59df49b65c7715
Your Name <you@example.com>
2024-09-06 20:41:04 +0000

    Change pre-apply 1725655264

--- plugins-fetch.nix
+++ plugins-fetch.nix
@@ -10,4 +10,19 @@ with pkgs.lib;
     plugins.nvim-quicker2.setup = mkOption { type = with types; str; };
   };
 
+  config = {
+    extraConfigLua = cfg.setup;
+    extraPlugins = [
+      # (pkgs.vimUtils.buildVimPlugin {
+      #   name = "nvim-quicker";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "stevearc";
+      #     repo = "quicker.nvim";
+      #     rev = "308088ebcec33f9ed551714e52390206b8f62ed6";
+      #     hash = "sha256-l2M4uVuQ+NW/Nf6fwGlBUqKiWzTld/tePMPMqk3W/oM=";
+      #   };
+      # })
+    ];
+    #config contents
+  };
 }
