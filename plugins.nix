11dead4e35d8460af45a0904c7486fbf065dd690
Your Name <you@example.com>
2024-09-06 08:16:54 +0000

    Change pre-apply 1725610614

--- plugins.nix
+++ plugins.nix
@@ -17,8 +17,8 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "stevearc";
       repo = "quicker.nvim";
-      rev = "1e2376ac6966805be9967f4ea0e4cf7c750f8214";
-      hash = "sha256-sDWRAiM50jjUzzXLYoxn8rcE+H088BeAkvXNUb1AaBM=";
+      rev = "308088ebcec33f9ed551714e52390206b8f62ed6";
+      hash = "sha256-l2M4uVuQ+NW/Nf6fwGlBUqKiWzTld/tePMPMqk3W/oM=";
     };
   };
 in
