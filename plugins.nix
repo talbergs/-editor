f5fbad8067ba9c6df5ddb2898f9e06345cd952dc
Your Name <you@example.com>
2024-09-08 11:36:53 +0000

    easy plugins

--- plugins.nix
+++ plugins.nix
@@ -5,14 +5,35 @@
 # https://github.com/jmbuhr/otter.nvim
 {
   pkgs,
+  lib,
   config,
   helpers,
   ...
 }:
-{
-  config.extraPlugins = [ pkgs.vimPlugins.tabby-nvim ];
-
-  config.extraConfigLua = ''
+with lib;
+let
+  tabby = pkgs.vimPlugins.tabby-nvim;
+  tabby_lua = ''
+    line = function(line)
+      return {
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          return {
+            line.sep('|', hl, theme.fill),
+            tab.is_current() and '▶' or '▷',
+            tab.name(),
+            line.sep(' ', hl, theme.fill),
+            hl = hl,
+            margin = ' ',
+          }
+        end),
+        line.spacer(),
+        hl = theme.fill,
+      }
+    end
+    require('tabby').setup({ line = line })
+  '';
+  user_lua = ''
     vim.cmd[[
     function! SourceFileIfExists(filepath) abort
         try
@@ -33,41 +54,14 @@
       win = 'TabLine',
       tail = 'TabLine',
     }
-    require('tabby').setup({
-      line = function(line)
-        return {
-          line.tabs().foreach(function(tab)
-            local hl = tab.is_current() and theme.current_tab or theme.tab
-            return {
-              line.sep('|', hl, theme.fill),
-              tab.is_current() and '▶' or '▷',
-              tab.name(),
-              line.sep(' ', hl, theme.fill),
-              hl = hl,
-              margin = ' ',
-            }
-          end),
-          line.spacer(),
-          -- line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-            -- return {
-              -- line.sep('|', theme.win, theme.fill),
-              -- win.is_current() and '◆' or '◇',
-              -- win.buf_name(),
-              -- line.sep(' ', theme.win, theme.fill),
-              -- hl = theme.win,
-              -- margin = ' ',
-            -- }
-          -- end),
-          -- {
-            -- line.sep(' ', theme.tail, theme.fill),
-            -- { '   ', hl = theme.tail },
-          -- },
-          hl = theme.fill,
-        }
-      end,
-      -- option = {}, -- setup modules' option,
-    })
   '';
+in
+{
+  config.extraPlugins = [ tabby ];
+  config.extraConfigLua = concatLines [
+    tabby_lua
+    user_lua
+  ];
   config.plugins = {
     undotree = {
       enable = true;
@@ -148,7 +142,6 @@
       servers.pyright.enable = true;
     };
     navic.enable = true;
-    # https://github.com/piersolenski/wtf.nvim
     wtf.enable = true;
     sidebar.enable = true;
     sidebar.setup = ''
